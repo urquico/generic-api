496f339299b570907f0c202a42d96e488d41341e
kurturquico <kurt.urquico@simplevia.com>
2025-05-23 20:23:37 +0800

    feature: added permission route

--- Controllers/PermissionsController.cs
+++ Controllers/PermissionsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using GenericApi.Utils;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace GenericApi.Controllers
 {
@@ -13,6 +14,49 @@ namespace GenericApi.Controllers
     {
         private readonly CustomSuccess _response = new();
 
-        // GET: /permissions/all?{query} - Get all permissions
+        /**
+         * GetAllPermissions endpoint retrieves all permissions.
+         *
+         * @returns {IActionResult} 200 if permissions retrieved successfully, 500 if an error occurred.
+         * @route GET /all
+         * @example response - 200 - Permissions retrieved successfully
+         * {
+         *   "statusCode": 200,
+         *   "message": "Permissions have been retrieved successfully.",
+         *   "data": null
+         * }
+         * @example response - 500 - Error
+         * {
+         *   "statusCode": 500,
+         *   "error": "An error occurred while retrieving permissions."
+         * }
+        */
+
+        [HttpGet("all")]
+        [ProducesResponseType(typeof(void), 200)]
+        [ProducesResponseType(typeof(object), 500)]
+        [SwaggerOperation(Summary = "Retrieve all permissions.")]
+        public IActionResult GetAllPermissions()
+        {
+            try
+            {
+                // TODO: Implement the logic to retrieve all permissions
+
+                const string activity = "Permissions have been retrieved successfully.";
+                string ip = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown IP";
+
+                return _response.Success(
+                    statusCode: 200,
+                    activity: activity,
+                    ip: ip,
+                    message: activity,
+                    data: null
+                );
+            }
+            catch (Exception ex)
+            {
+                return _response.Error(statusCode: 500, e: ex);
+            }
+        }
     }
 }
