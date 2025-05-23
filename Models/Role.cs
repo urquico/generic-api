74ca6a4dc573d4e80bcd6b0846c8d31b72982cbc
kurturquico <kurt.urquico@simplevia.com>
2025-05-24 00:02:14 +0800

    feature: added models and db context

---
+++ Models/Role.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericApi.Models;
+
+public partial class Role
+{
+    public int Id { get; set; }
+
+    public string RoleName { get; set; } = null!;
+
+    public int? RoleStatus { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public int? UpdatedBy { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public int? DeletedBy { get; set; }
+
+    public virtual ICollection<RolePermission> RolePermissions { get; set; } = new List<RolePermission>();
+}
