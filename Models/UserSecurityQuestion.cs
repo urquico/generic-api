3b071ce1db83ceb8bbd9127faa8790dc247c78ef
Kurt Jacob Urquico <kurt.urquico@simplevia.com>
2025-05-24 22:52:10 +0800

    feature: added initial sql schemas and tables (#6)
    * feature: added sql schema and tables
    
    * feature: added models and db context
    
    * misc: updated cspell.json

---
+++ Models/UserSecurityQuestion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericApi.Models;
+
+public partial class UserSecurityQuestion
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public int SecurityQuestionId { get; set; }
+
+    public string SecurityAnswer { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public int? UpdatedBy { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public int? DeletedBy { get; set; }
+
+    public virtual SecurityQuestion SecurityQuestion { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ Models/UserSpecialPermission.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericApi.Models;
+
+public partial class UserSpecialPermission
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public int PermissionId { get; set; }
+
+    public int? AccessStatus { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public int? UpdatedBy { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public int? DeletedBy { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
