5bb01e9d5c5b06202f18bde7561f3dff6600a8e1
kurturquico <kurt.urquico@simplevia.com>
2025-06-04 20:03:13 +0800

    feature: added get single role

---
+++ Utils/Roles/GetAllRolesMessages.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericApi.Utils.Roles
+{
+    public class GetAllRolesMessages
+    {
+        public const string SUCCESS = "Roles retrieved successfully.";
+    }
+}
--- Utils/SwaggerSummary/RoleSummary.cs
+++ Utils/SwaggerSummary/RoleSummary.cs
@@ -10,5 +10,6 @@ namespace GenericApi.Utils.SwaggerSummary
         public const string CREATE = "Create a new role with permissions";
         public const string UPDATE = "Update an existing role with permissions";
         public const string GET_ALL = "Get all roles with pagination";
+        public const string GET_SINGLE_ROLE = "Get a single role by ID with permissions";
     }
 }
